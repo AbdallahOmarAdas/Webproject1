ac102f0fc2e94f183f1190cfbf3e86e92f6538c0
AbdallahOmarAdas <s11924993@stu.najah.edu>
2022-04-19 05:03:21 +0300

    date19_5.30

---
+++ manger.php
@@ -0,0 +1,69 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/mangerCSS.css">
+    <script src="node_modules/@fortawesome/fontawesome-free/js/brands.js"></script>
+    <script src="node_modules/@fortawesome/fontawesome-free/js/solid.js"></script>
+    <script src="node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"></script>
+    <title>Hello, world!</title>
+</head>
+<body>
+<div class="container-xxl">
+    <div class="row">
+        <div id="side1" class="col-3" style="background: url('images/best/sidebar-5.jpg')no-repeat fixed; height: 731px">
+             <div id="fixed_div" class="container container-fluid d-flex justify-content-center"style="background-color: rgb(0,128,255);">
+                 <div style="position: fixed;">
+                 <form action="">
+                     <table>
+                         <tr>
+                             <td>
+                                 <img src="images/logo1.png" alt="" ><br>
+                                 <span>Alhijaz chocolate LTD</span>
+                             </td>
+                         </tr>
+                         <tr>
+                             <td>
+                                 <input class="side_input" type="button" value="Page management">
+                             </td>
+                         </tr>
+                         <tr>
+                             <td>
+                                 <input class="side_input" type="button" value="employees management">
+                             </td>
+                         </tr>
+                         <tr>
+                             <td>
+                                 <input class="side_input" type="button" value="orders management">
+                             </td>
+                         </tr>
+                     </table>
+
+                 </form>
+                 </div>
+             </div>
+        </div>
+
+        <div class="col-9">
+            <div class="navbar">
+                <div class="container">
+                    Alhijaz chocolate LTD
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
